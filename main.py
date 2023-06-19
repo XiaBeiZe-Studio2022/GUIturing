b872f967896020ed9111b9bdacb28c51c7f6bf02
cmc2012 <1042286543@qq.com>
2023-06-19 22:15:42 +0800

    Update

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-'''
+"""
+       _            _       _     _         _                 _          _           _            _              _                   _          _   _                 _         _                _            _       _     _        _           _
+      /\ \         / /\    / /\  /\ \      / /\              /\ \       /\ \        /\ \         /\ \           / /\                /\ \       /\_\/\_\ _            /\ \      / /\             / /\         / /\    / /\  /\ \     /\ \        /\_\
+      \_\ \       / / /   / / /  \ \ \    / /  \            /  \ \     /  \ \      /  \ \       /  \ \         / /  \              /  \ \     / / / / //\_\          \ \ \    / /  \           / /  \       / / /   / / /  \ \ \    \_\ \      / / /
+      /\__ \     / /_/   / / /   /\ \_\  / / /\ \__        / /\ \ \   / /\ \ \    / /\ \ \     / /\ \_\       / / /\ \            / /\ \ \   /\ \/ \ \/ / /          /\ \_\  / / /\ \__       / / /\ \__   / /_/   / / /   /\ \_\   /\__ \    / / /_
+     / /_ \ \   / /\ \__/ / /   / /\/_/ / / /\ \___\      / / /\ \_\ / / /\ \_\  / / /\ \ \   / / /\/_/      / / /\ \ \          / / /\ \_\ /  \____\__/ /          / /\/_/ / / /\ \___\     / / /\ \___\ / /\ \__/ / /   / /\/_/  / /_ \ \  / /___/\
+    / / /\ \ \ / /\ \___\/ /   / / /    \ \ \ \/___/     / / /_/ / // / /_/ / / / / /  \ \_\ / / / ______   / / /  \ \ \        / / /_/ / // /\/________/          / / /    \ \ \ \/___/     \ \ \ \/___// /\ \___\/ /   / / /    / / /\ \ \ \____ \ \
+   / / /  \/_// / /\/___/ /   / / /      \ \ \          / / /__\/ // / /__\/ / / / /   / / // / / /\_____\ / / /___/ /\ \      / / /__\/ // / /\/_// / /          / / /      \ \ \            \ \ \     / / /\/___/ /   / / /    / / /  \/_/     / / /
+  / / /      / / /   / / /   / / /   _    \ \ \        / / /_____// / /_____/ / / /   / / // / /  \/____ // / /_____/ /\ \    / / /_____// / /    / / /          / / /   _    \ \ \       _    \ \ \   / / /   / / /   / / /    / / /           / / /
+ / / /      / / /   / / /___/ / /__ /_/\__/ / /       / / /      / / /\ \ \  / / /___/ / // / /_____/ / // /_________/\ \ \  / / /\ \ \ / / /    / / /       ___/ / /__ /_/\__/ / /      /_/\__/ / /  / / /   / / /___/ / /__  / / /           _\/_/
+/_/ /      / / /   / / //\__\/_/___\\ \/___/ /       / / /      / / /  \ \ \/ / /____\/ // / /______\/ // / /_       __\ \_\/ / /  \ \ \\/_/    / / /       /\__\/_/___\\ \/___/ /       \ \/___/ /  / / /   / / //\__\/_/___\/_/ /           /\_\
+\_\/       \/_/    \/_/ \/_________/ \_____\/        \/_/       \/_/    \_\/\/_________/ \/___________/ \_\___\     /____/_/\/_/    \_\/        \/_/        \/_________/ \_____\/         \_____\/   \/_/    \/_/ \/_________/\_\/            \/_/
 
 
-'''
+
+"""
 
 import json
+import logging
 import os
+import platform
 import random
 import shutil
 import time
 import urllib.parse
-import platform
 import zipfile
+from logging import handlers
 
 import dearpygui.dearpygui as dpg
 import urllib3
@@ -20,9 +34,25 @@ from Tools import download_project
 
 http = urllib3.PoolManager()
 
+logger = logging.getLogger("logger1")
+logger.setLevel(level=logging.DEBUG)
+
+format = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+
+file_handler = handlers.TimedRotatingFileHandler(filename="./log/log.log", when="D")
+file_handler.setLevel(level=logging.DEBUG)
+file_handler.setFormatter(format)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(format)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
 dpg.create_context()
 
-with open("./Shit/"+str(random.randint(1,10))+".txt") as shit:
+with open("./Shit/" + str(random.randint(1, 10)) + ".txt") as shit:
     print(shit.read())
 # 读取配置
 with open("config.json", "rb+") as file:
@@ -36,25 +66,28 @@ with dpg.font_registry():
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
     dpg.bind_font(font1)
 
+
 def download_vscode():
-    dpg.configure_item("downloading",show=True)
+    dpg.configure_item("downloading", show=True)
     try:
-        vscodium=http.request("GET","https://ydschool-online.nosdn.127.net/svg/Vscode.mp3")
+        vscodium = http.request("GET", "https://ydschool-online.nosdn.127.net/svg/Vscode.mp3")
     except:
         raise Exception("Time Out")
-    with open("~temp~","wb+") as file:
+    with open("~temp~", "wb+") as file:
         file.write(vscodium.data)
-    dpg.configure_item("downloading",show=False)
+    dpg.configure_item("downloading", show=False)
     time.sleep(0.1)
     dpg.configure_item("unzip", show=True)
     with zipfile.ZipFile("~temp~") as zf:
         zf.extractall("vscode")
-    dpg.configure_item("unzip",show=False)
+    dpg.configure_item("unzip", show=False)
     time.sleep(0.1)
-    dpg.configure_item("complete",show=True)
+    dpg.configure_item("complete", show=True)
     os.remove("~temp~")
-if(not(platform.platform()!="Windows")):
-    with dpg.window(label="不支持",no_close=True):
+
+
+if (not (platform.platform() != "Windows")):
+    with dpg.window(label="不支持", no_close=True):
         dpg.add_text("不支持的系统")
     dpg.create_viewport(title="Error")
     dpg.setup_dearpygui()
@@ -62,22 +95,23 @@ if(not(platform.platform()!="Windows")):
     dpg.start_dearpygui()
     dpg.destroy_context()
 
-if(not(os.path.exists("./vscode"))):
+if (not (os.path.exists("./vscode"))):
     with dpg.window(label="缺少vscode"):
-        dpg.add_button(label="下载",callback=download_vscode)
-    with dpg.window(label="下载中",modal=True,show=False,tag="downloading",no_close=True):
+        dpg.add_button(label="下载", callback=download_vscode)
+    with dpg.window(label="下载中", modal=True, show=False, tag="downloading", no_close=True):
         dpg.add_text("下载中")
         dpg.add_loading_indicator(style=1)
-    with dpg.window(label="解压中",modal=True,show=False,tag="unzip",no_close=True):
+    with dpg.window(label="解压中", modal=True, show=False, tag="unzip", no_close=True):
         dpg.add_text("解压中")
         dpg.add_loading_indicator(style=1)
-    with dpg.window(label="下载完成",modal=True,show=False,tag="complete",no_close=True):
+    with dpg.window(label="下载完成", modal=True, show=False, tag="complete", no_close=True):
         dpg.add_text("下载完成")
     dpg.create_viewport(title="Error")
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
     dpg.destroy_context()
+
 
 def get_project():
     url = dpg.get_value("DownloadUrl")
@@ -103,8 +137,10 @@ def debug():
 
 
 def runBot(user_data):
-    path="./Bots/"+user_data
-    path=os.path.abspath(path)
+    path = "./Bots/" + user_data
+    path = os.path.abspath(path)
+
+
 def closeErrorWindow():
     time.sleep(0.1)
     dpg.configure_item("errorCookie", show=False)
@@ -112,8 +148,17 @@ def closeErrorWindow():
     dpg.configure_item("login", show=True)
 
 
+def openBotWindow():
+    if (not (rightCookie)):
+        with dpg.window(label="没有登录", modal=True):
+            dpg.add_text("没有登录")
+            return 0
+    dpg.configure_item("bot", show=True)
+
+
 def getInfo():
     info = GetInfo.getInfo(dpg.get_value("cookie"))
+    global rightCookie
     rightCookie = True
     try:
         userId = info["data"]["userId"]
@@ -122,7 +167,7 @@ def getInfo():
         dpg.configure_item("login", show=False)
         time.sleep(0.1)
         dpg.configure_item("errorCookie", show=True)
-    except Exception as e:
+    except:
         raise
     if (rightCookie):
         dpg.configure_item("mainMenu", show=True)
@@ -131,8 +176,8 @@ def getInfo():
 
 with dpg.window(label="机器人", show=False, tag="bot"):
     bots = os.listdir(".\\Bots")
-    dpg.add_listbox(items=bots,label="请选择一个机器人",tag="chooseBot")
-    dpg.add_button(label="确定",tag="runBot",user_data=dpg.get_value("chooseBot"))
+    dpg.add_listbox(items=bots, label="请选择一个机器人", tag="chooseBot")
+    dpg.add_button(label="确定", tag="runBot", user_data=dpg.get_value("chooseBot"))
 
 with dpg.window(label=config["toolsMenu"]["title"], tag="tools", show=False):
     dpg.add_button(label=config["toolsMenu"]["tools"][0])
@@ -153,11 +198,11 @@ with dpg.window(label=config["toolsMenu"]["title"], tag="tools", show=False):
 with dpg.window(tag="mainMenu", label="GUIturing主菜单", no_close=True, width=200, height=500, show=False):
     dpg.add_button(label=config["mainMenu"]["debug"], callback=debug)
     dpg.add_button(label="工具", callback=lambda: dpg.configure_item("tools", show=True))
-    dpg.add_button(label="机器人",callback=lambda : dpg.configure_item("bot",show=True))
+    dpg.add_button(label="机器人", callback=openBotWindow)
 with dpg.window(tag="errorCookie", label="错误的cookie", modal=True, show=False):
     dpg.add_text(default_value="错误的cookie")
     dpg.add_button(label="确定", callback=closeErrorWindow)
-# with dpg.window(tag="runBot",label="运行机器人")
+
 with dpg.window(tag="login", label="登录", no_close=True, modal=True, height=300, width=500):
     dpg.add_text("输入cookie以登录小图灵账号")
     with dpg.value_registry():
